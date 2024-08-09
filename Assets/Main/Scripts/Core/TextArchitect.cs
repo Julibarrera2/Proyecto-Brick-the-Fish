11980094b20aaa76d5ff9957813378454c9afaec
Talizzxz <148806451+Talizzxz@users.noreply.github.com>
2024-08-09 18:58:56 -0300

    ERASEEVERYTHING

--- Assets/Main/Scripts/Core/TextArchitect.cs
+++ Assets/Main/Scripts/Core/TextArchitect.cs
@@ -20,4 +20,43 @@ public class TextArchitect
 
     public enum BuildMethod { instant, typewriter, fade }
     public BuildMethod buildMethod = BuildMethod.typewriter;
+
+    public Color textColor { get { return tmpro.color; } set { tmpro.color = value; } }
+
+    public float speed { get { return baseSpeed * speedMultiplier; } set { speedMultiplier = value; } }
+    private const baseSpeed = 1;
+    private float speedMultiplier = 1;
+
+    public int charactersPerCycle { get { return speed <= 2f ? characterMultiplier : speed <= 2.5f ? characterMultiplier * 2 : characterMultiplier * 3} }
+    private int characterMultiplier = 1;
+
+    public bool hurryUp = false;
+
+    public TextArchitect(TextMeshProUGUI tmpro_ui)
+    {
+        this.tmpro_ui = tmpro_ui;
+    }
+    public TextArchitect(TextMeshProUGUI tmpro_world)
+    {
+        this.tmpro_world = tmpro_world;
+    }
+
+    public Coroutine Build(string text)
+    {
+        preText = "";
+        targetText = text;
+
+        Stop();
+    }
+
+    private Coroutine buildProcess = null;
+    public bool isBuilding => buildProcess != null;
+
+    public void Stop()
+    {
+        if (!isBuilding)
+            return;
+        tmpro.StopCoroutine(buildProcess);
+        buildProcess = null;
+    }
 }
