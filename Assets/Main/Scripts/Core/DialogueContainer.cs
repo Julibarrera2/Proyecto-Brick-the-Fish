61b36a191afbb121dfbb02443cc11dbcd023810c
Talizzxz <148806451+Talizzxz@users.noreply.github.com>
2024-08-09 10:42:37 -0300

    script

---
+++ Assets/Main/Scripts/Core/DialogueContainer.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using TMPro;
+
+[System.Serializable]
+public class DialogueContainer
+{
+    public GameObject root;
+    public TMP_Text nameText;
+    public TMP_Text dialogueText;
+}
---
+++ Assets/Main/Scripts/Core/TextArchitect.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+using TMPro;
+
+public class TextArchitect
+{
+    private TextMeshProUGUI tmpro_ui;
+    private TextMeshPro tmpro_world;
+
+    public TMP_Text tmpro => tmpro_ui != null ? tmpro_ui : tmpro_world;
+
+    public string currentText => tmpro.text;
+
+    public string targetText { get; private set; } = "";
+    public string preText { get; private set; } = "";
+
+    private int preTextLenght = 0;
+
+    public string fullTargetText => preText + targetText;
+
+    public enum BuildMethod { instant, typewriter, fade }
+    public BuildMethod buildMethod = BuildMethod.typewriter;
+}
