a1508b956537e57b04594224079a6cd01df91d0b
Talizzxz <148806451+Talizzxz@users.noreply.github.com>
2024-08-09 21:30:46 -0300

    empezamos devuelta😍

--- Assets/Main/Scripts/Core/Speaker.cs
+++ Assets/Main/Scripts/Core/Speaker.cs
@@ -2,9 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DialogueSystem : MonoBehaviour
+public class Speaker : MonoBehaviour
 {
-    [SerializeField] private DialogueContainer dialogueContainer = new DialogueContainer();
     // Start is called before the first frame update
     void Start()
     {
