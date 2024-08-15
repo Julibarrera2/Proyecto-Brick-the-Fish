7f617d76fe68b72574467ea7501d9d001eb93a50
Talizzxz <148806451+Talizzxz@users.noreply.github.com>
2024-08-15 14:28:35 -0300

    adsasc

--- Assets/Main/Scripts/Core/BackgroundController.cs
+++ Assets/Main/Scripts/Core/BackgroundController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class SpriteSwitcher : MonoBehaviour
+public class BackgroundController : MonoBehaviour
 {
     public bool IsSwitched = false;
     public Image background1;
