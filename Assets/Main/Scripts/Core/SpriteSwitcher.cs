b3d765eb5858010d63f6331c2250f0bfdd37ed62
Talizzxz <148806451+Talizzxz@users.noreply.github.com>
2024-08-14 23:33:28 -0300

    sprites

--- Assets/Main/Scripts/Core/SpriteSwitcher.cs
+++ Assets/Main/Scripts/Core/SpriteSwitcher.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class BackgroundController : MonoBehaviour
+public class SpriteSwitcher : MonoBehaviour
 {
     public bool IsSwitched = false;
     public Image background1;
