0df3a5f2c73f49ef9f219bb37adab54b4e3c9bee
Talizzxz <148806451+Talizzxz@users.noreply.github.com>
2024-09-02 22:00:26 -0300

    prototipo juego 3d ponele que terminado

---
+++ Assets/3d game/Scripts/CharacterMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public float horizontalInput;
+    public float verticalInput;
+    public float turnSpeed = 10;
+    public float movespeed = 3f;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * verticalInput);
+        transform.Translate(Vector3.right * Time.deltaTime * horizontalInput * movespeed);
+
+        transform.Translate(Vector3.forward * Time.deltaTime * verticalInput * movespeed);
+
+        transform.Rotate(Vector3.up * horizontalInput * turnSpeed * Time.deltaTime);
+
+    }
+}
