0e121fe803f0b3634e6178400205ad78d06de8df
Talizzxz <148806451+Talizzxz@users.noreply.github.com>
2024-09-16 23:51:54 -0300

    j

--- Assets/3d game/Scripts/TotaMovement.cs
+++ Assets/3d game/Scripts/TotaMovement.cs
@@ -18,55 +18,21 @@ public class TotaMovement : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.W))
         {
-            anim.SetBool("camina", true);
+            anim.SetBool("walking", true);
         }
         if (Input.GetKeyUp(KeyCode.W))
         {
-            anim.SetBool("camina", false);
+            anim.SetBool("walking", false);
         }
         if (Input.GetKeyDown(KeyCode.S))
         {
-            anim.SetBool("retrocede", true);
+            anim.SetBool("walking", true);
         }
         if (Input.GetKeyUp(KeyCode.S))
         {
-            anim.SetBool("retrocede", false);
+            anim.SetBool("walking", false);
         }
-
-
-        if (!anim.GetBool("camina") && !anim.GetBool("retrocede"))
-        {
-            if (Input.GetKeyDown(KeyCode.A))
-            {
-                anim.SetBool("giraIzq", true);
-            }
-            if (Input.GetKeyUp(KeyCode.A))
-            {
-                anim.SetBool("giraIzq", false);
-            }
-            if (Input.GetKeyDown(KeyCode.D))
-            {
-                anim.SetBool("giraDer", true);
-            }
-            if (Input.GetKeyUp(KeyCode.D))
-            {
-                anim.SetBool("giraDer", false);
-            }
-        }
-        else
-        {
-            anim.SetBool("giraDer", false);
-            anim.SetBool("giraIzq", false);
-            if (Input.GetKey(KeyCode.A))
-            {
-                transform.Rotate(0, velRotacion, 0);
-            }
-            if (Input.GetKey(KeyCode.D))
-            {
-                transform.Rotate(0, -velRotacion, 0);
-            }
-        }
-
 
     }
+        
 }
