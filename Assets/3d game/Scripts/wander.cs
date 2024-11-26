caff8f3113606981fad8df3fbabca7e201ad9bf0
Julibarrera2 <164010054+Julibarrera2@users.noreply.github.com>
2024-11-26 10:51:55 -0300

    olivia 2 la puta q te pario

---
+++ Assets/3d game/Scripts/wander.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class wander : MonoBehaviour
+{
+    NavMeshAgent agent;
+    public Vector3[] corners;
+    void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+    
+    void Start()
+    {
+        agent.destination = new Vector3(Random.Range(corners[0].x, corners[1].x), transform.position.y, Random.Range(corners[0].z, corners[1].z));
+        GetComponent<Animator>().SetBool("walking", true);
+    }
+    void Update()
+    {
+        if (agent.remainingDistance <= agent.stoppingDistance)
+            agent.destination = new Vector3(Random.Range(corners[0].x, corners[1].x), transform.position.y, Random.Range(corners[0].z, corners[1].z));
+    }
+}
