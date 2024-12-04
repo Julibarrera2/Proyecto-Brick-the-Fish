cd821e14dd014c5437f7f87d3e48bf26337743cf
Talizzxz <148806451+Talizzxz@users.noreply.github.com>
2024-12-03 22:18:07 -0300

    homescreen

---
+++ Assets/SceneManagement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagement : MonoBehaviour
+{
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(0, LoadSceneMode.Single);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Game Quit!");
+    }
+}
